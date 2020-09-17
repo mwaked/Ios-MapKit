baf27babd3377faffa9f3c74fd0034b7c2e509cb
Mahmoud Waked <mwaked@gmail.com>
2020-09-17 21:04:13 +0200

    uplaod the simple

--- MapKitExample/ViewController.swift
+++ MapKitExample/ViewController.swift
@@ -7,14 +7,45 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate  {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+         // 1
+        let location = CLLocationCoordinate2D(latitude: 51.50007773,
+            longitude: -0.1246402)
+        
+        // 2
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+            mapView.setRegion(region, animated: true)
+            
+        //3
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        mapView.addAnnotation(annotation)
+        
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            if status == .authorizedAlways {
+                if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+                    if CLLocationManager.isRangingAvailable() {
+                        // do stuff
+                    }
+                }
+            }
+        }
 
 }
 
