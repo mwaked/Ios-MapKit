85f83204c68689a92b3832f38ba118f008b89aa4
Mahmoud Waked <mwaked@gmail.com>
2020-09-17 20:27:52 +0200

    Initial Commit

---
+++ MapKitExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKitExample
+//
+//  Created by Mahmoud Waked on 9/17/20.
+//  Copyright © 2020 BIM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
